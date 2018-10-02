6fb379daf473aebec97f29f872df5bd10aa1ede6
Ethan Burda <burdaethan3@gmail.com>
2018-10-02 07:58:49 -0700

    Twenty One Game (Blackjack) Console App
    This is my first submission of the Twenty One Game Console App.

---
+++ TwentyOne/Casino/Dealer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Casino
+{
+    public class Dealer
+    {
+        public string Name { get; set; } //property
+        public Deck Deck { get; set; } //property
+        public int Balance { get; set; } //property
+
+        public void Deal(List<Card> Hand) //method
+        {
+            Hand.Add(Deck.Cards.First()); //Taking the top (first) card in the deck and putting in dealers hand.
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+           using (StreamWriter file = new StreamWriter(@"C:\User\Student\Logs\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
+            Deck.Cards.RemoveAt(0);
+        }
+    }
+}
